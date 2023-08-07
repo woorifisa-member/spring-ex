6b970fee5ab3288dbfb4fe4666e73df632e90872
Dongyeol <eastheat10@gmail.com>
2023-08-07 23:17:58 +0900

    login test with session

--- src/test/java/com/woorifisa/board/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/woorifisa/board/domain/member/controller/MemberControllerTest.java
@@ -1,7 +1,9 @@
 package com.woorifisa.board.domain.member.controller;
 
 import static java.nio.charset.StandardCharsets.*;
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.http.MediaType.*;
@@ -16,14 +18,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.woorifisa.board.common.dto.session.MemberSession;
 import com.woorifisa.board.domain.dto.member.request.DummySignupRequest;
+import com.woorifisa.board.domain.member.dto.request.LoginRequest;
 import com.woorifisa.board.domain.member.dto.request.SignupRequest;
 import com.woorifisa.board.domain.member.dto.response.MemberResponse;
 import com.woorifisa.board.domain.member.service.MemberService;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @WebMvcTest(MemberController.class)
 class MemberControllerTest {
@@ -56,7 +64,7 @@ class MemberControllerTest {
 		DummySignupRequest dummySignupRequest = new DummySignupRequest(email, password, name);
 		String requestBody = om.writeValueAsString(dummySignupRequest);
 
-		// System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(dummySignupRequest));
+		System.out.println(om.writerWithDefaultPrettyPrinter().writeValueAsString(dummySignupRequest));
 
 		given(memberService.signup(any(SignupRequest.class))).willReturn(memberResponse);
 
@@ -67,8 +75,58 @@ class MemberControllerTest {
 			.andExpect(jsonPath("$.code", is(HttpStatus.OK.value())))
 			.andExpect(jsonPath("$.data.id", is(id), Long.class))
 			.andExpect(jsonPath("$.data.email", is(email)));
-
 		then(memberService).should(times(1)).signup(any(SignupRequest.class));
+	}
+
+	@DisplayName("로그인")
+	@Test
+	void testLogin() throws Exception {
+
+		Long id = 1L;
+		String email = "email@email.com";
+		String password = "password";
+		String name = "홍길동";
+
+		MockHttpSession session = spy(new MockHttpSession());
+
+		// HttpSession mockSession = mock(HttpSession.class);
+		// willDoNothing().given(mockSession).setAttribute("hello", "world");
+
+		DummyLoginRequest req = new DummyLoginRequest(email, password);
+		MemberResponse memberResponse = new MemberResponse(id, email, name);
+
+		String requestBody = om.writeValueAsString(req);
+		given(memberService.login(any(LoginRequest.class))).willReturn(memberResponse);
+
+		mockMvc.perform(post("/members/login")
+				.session(session)
+				.characterEncoding(UTF_8)
+				.contentType(APPLICATION_JSON)
+				.content(requestBody))
+
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(APPLICATION_JSON))
+			.andExpect(jsonPath("$.code", is(HttpStatus.OK.value())))
+			.andExpect(jsonPath("$.data.id", is(id), Long.class))
+			.andExpect(jsonPath("$.data.email", is(email)))
+			.andExpect(jsonPath("$.data.name", is(name)));
+
+		MemberSession attribute = (MemberSession)session.getAttribute(MemberSession.KEY);
+
+		assertAll(
+			() -> assertThat(attribute).isNotNull(),
+			() -> assertThat(attribute.getEmail()).isEqualTo(email)
+		);
+
+		then(memberService).should(times(1)).login(any(LoginRequest.class));
+		then(session).should(times(1)).setAttribute(anyString(), any(MemberSession.class));
+	}
+
+	@AllArgsConstructor
+	@Getter
+	static class DummyLoginRequest {
+		String email;
+		String password;
 	}
 
 }
